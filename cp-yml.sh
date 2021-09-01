a50ccb5ba46177cd45e4d8500f3332a1edf032fd
johnwu94 <johnwu94@hotmail.com>
2021-09-02 07:18:48 +1000

    test

---
+++ cp-yml.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir ../allyml
+
+for i in ./*; do
+    #echo "$i"
+    if [ -d "$i" ]; then
+        echo "$i is a dir" ;
+        cp $i/*.yml ../allyml/
+    else
+        echo "$i is not a dir"
+    fi
+done
+~      
